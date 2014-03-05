50ad0ad98a61168b42b2c721cc34b59e996f6187
Anas Khallouf <anas-kh@hotmail.fr>
2014-03-05 10:47:22 +0100

    Connect to Database

--- rUonTime/src/com/android/ruontime/Login.java
+++ rUonTime/src/com/android/ruontime/Login.java
@@ -1,21 +1,44 @@
 package com.android.ruontime;
 
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.NameValuePair;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.message.BasicNameValuePair;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+
 import android.app.Activity;
+import android.app.ProgressDialog;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.view.View.OnClickListener;
 import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.Toast;
 
-public class Login extends Activity {
+public class Login extends Activity implements OnClickListener{
 	
-	EditText username , password ; 
-	Button login,reset ;
-	CheckBox remember;
+	private EditText user, pass;
+	private Button  mSubmit, mRegister;
+	private CheckBox remember;
+	private Boolean succ = false;
+    private ProgressDialog pDialog;
 	SharedPreferences preferences ;
 	String PREFS_NAME = "com.example.sp.LoginPrefs";
 
@@ -23,55 +46,145 @@ public class Login extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.login_screen);
-	
-		username = (EditText)findViewById(R.id.UsernameEd);
-		password = (EditText)findViewById(R.id.PasswordEd);
-		login = (Button)findViewById(R.id.login_button);
+
+		user = (EditText) findViewById(R.id.UsernameEd);
+		pass = (EditText) findViewById(R.id.PasswordEd);
+
+		mSubmit = (Button) findViewById(R.id.login_button);
+		mRegister = (Button) findViewById(R.id.register);
 		remember = (CheckBox)findViewById(R.id.remember_checkBox);
+
 		
 		preferences = getSharedPreferences(PREFS_NAME, Context.MODE_PRIVATE);
 		if (preferences.getString("logged", "").toString().equals("logged")) 
 		{
 			Intent i = new Intent(Login.this,MyCalendars.class);
-			i.putExtra("USERNAME",preferences.getString("username", "").toString());
-			i.putExtra("PASSWORD",preferences.getString("password", "").toString());
-			i.putExtra("CHECK", true);
 			startActivity(i);			
 		}
-		
-		login.setOnClickListener(new View.OnClickListener() {
-			
-			public void onClick(View arg0) {
-				// TODO Auto-generated method stub
-				if(!username.getText().toString().equals("") &&  (!password.getText().equals("")))
-				{
-					if(remember.isChecked())
-					{
-						SharedPreferences.Editor editor = preferences.edit();
-						editor.putString("username", username.getText().toString());
-						editor.putString("password", password.getText().toString());
-						editor.putString("logged", "logged");
-						editor.commit();
-					}
-					Intent i = new Intent(Login.this,MyCalendars.class);
-					i.putExtra("USERNAME", username.getText().toString());
-					i.putExtra("PASSWORD", password.getText().toString());
-					i.putExtra("CHECK", remember.isChecked());
-					startActivity(i);	
-					
-				}
-				else
-				{
-					Toast.makeText(Login.this,"Please enter both the fields.", Toast.LENGTH_SHORT).show();
-				}
-				
-			}
-		});
 
-}
+		mSubmit.setOnClickListener(this);
+		mRegister.setOnClickListener(this);
+	}
+
+	@Override
+	public void onClick(View v) {
+		switch (v.getId()) {
+		case R.id.login_button:
+			new AttemptLogin().execute();
+			break;
+		case R.id.register:
+			Intent i = new Intent(this, Register.class);
+			startActivity(i);
+			break;
+		default:
+			break;
+		}
+	}
+	
+	class AttemptLogin extends AsyncTask<String, String, String> {
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+            pDialog = new ProgressDialog(Login.this);
+            pDialog.setMessage("Atempting to login...");
+            pDialog.setIndeterminate(false);
+            pDialog.setCancelable(true);
+            pDialog.show();
+        }
+		
+		@Override
+		protected String doInBackground(String... args) {
+			
+			String result ="";
+			String success = "";
+        	String word = pass.getText().toString();
+        	String usern = user.getText().toString();
+			InputStream isr = null;
+	        try {
+	        	List<NameValuePair> params = new ArrayList<NameValuePair>();
+	            params.add(new BasicNameValuePair("username", usern));
+	            params.add(new BasicNameValuePair("password", word));
+	        	DefaultHttpClient httpClient = new DefaultHttpClient();
+                HttpPost httpPost = new HttpPost("http://130.229.129.70/webservice/login.php");
+           // 	HttpPost httpPost = new HttpPost("http://192.168.56.1/webservice/login.php");
+                httpPost.setEntity(new UrlEncodedFormEntity(params,"utf-8"));
+ 
+                HttpResponse httpResponse = httpClient.execute(httpPost);
+                HttpEntity httpEntity = httpResponse.getEntity();
+                isr = httpEntity.getContent();
+
+	        } catch (Exception e) {
+	            Log.e("Error Connection",e.toString());
+	        }
+	        try {
+	            BufferedReader reader = new BufferedReader(new InputStreamReader(
+	                    isr, "utf-8"), 8);
+	            StringBuilder sb = new StringBuilder();
+	            String line = null;
+	            while ((line = reader.readLine()) != null) {
+	                sb.append(line + "\n");
+	            }
+	            isr.close();
+	            result = sb.toString();
+	            Log.e("json String", result);
+	        } catch (Exception e) {
+	            Log.e("Buffer Error", "Error converting result " + e.toString());
+	        }
+	        
+	        try {
+	        	JSONObject json = new JSONObject(result);
+	        	success = json.getString("success");
+	        	Log.d("Register GOOD!", success);
+	        } catch (JSONException e) {
+	            Log.e("JSON Parser", "Error parsing data " + e.toString());
+	        }
+
+	        if (success.equals("1")){
+	        	Log.d("Register GOOD!", "working");
+	        	succ = true;
+	        	SharedPreferences.Editor editor = preferences.edit();
+	        	editor.putString("username", user.getText().toString());
+				editor.putString("password", pass.getText().toString());
+				editor.commit();
+				
+	            if(remember.isChecked())
+				{
+					editor.putString("logged", "logged");
+					editor.commit();
+				}       	
+	        	Intent i = new Intent(Login.this, MyCalendars.class);
+				startActivity(i);
+	        	
+	        	/*Intent i = new Intent(Login.this,Tabs.class);
+				i.putExtra("USERNAME", user.getText().toString());
+				i.putExtra("PASSWORD", pass.getText().toString());
+				i.putExtra("CHECK", remember.isChecked());
+				startActivity(i);*/
+	        /*	Intent i = new Intent(Login.this, MyCalendars.class);
+				startActivity(i);*/
+	        }
+	        else {
+				Log.d("Register Failure!", "shit");
+			}		
+              return null;
+		}
+		
+        protected void onPostExecute(String file_url) {
+
+            pDialog.dismiss();
+            if (!succ){
+            	Toast.makeText(Login.this, "Username/Password invalid, try again!", Toast.LENGTH_LONG).show();
+            }
+          /*  if (file_url != null){
+            	Toast.makeText(Login.this, file_url, Toast.LENGTH_LONG).show();
+            }
+ */
+        }		
+	}		 
 	@Override
 	protected void onPause() {
 		super.onPause();
-		finish();
+				finish();
 	}
 }
+
--- rUonTime/src/com/android/ruontime/Settings.java
+++ rUonTime/src/com/android/ruontime/Settings.java
@@ -2,6 +2,8 @@ package com.android.ruontime;
 
 import com.android.ruontime.service.Reminder;
 
+import android.content.Context;
+import android.content.Intent;
 import android.content.SharedPreferences;
 import android.content.SharedPreferences.OnSharedPreferenceChangeListener;
 import android.os.Bundle;
@@ -15,6 +17,18 @@ public class Settings extends PreferenceActivity implements OnSharedPreferenceCh
 
 	public static final String ACTIVATE_SERVICE = "activateService";
 	public static final String REMINDER = "reminder";
+	public static final String LOGOUT = "log_out";
+	public static final String GROUPS = "groups";
+	public static final String DROP = "drop";
+	public static final String CHANGE = "change";
+	
+	public static final String DIGEST_LIST = "digest_list";
+	public static final String DIGEST_CHECKBOX = "activateDigest";
+	
+	SharedPreferences pr ;
+	String PREFS_NAME = "com.example.sp.LoginPrefs";
+	String CALENDAR_CHOSEN = "chooseYourCalendar";
+
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -24,7 +38,54 @@ public class Settings extends PreferenceActivity implements OnSharedPreferenceCh
 		Preference pref = findPreference(REMINDER);
 		SharedPreferences sharedPreferences=PreferenceManager.getDefaultSharedPreferences(getBaseContext());
 		pref.setSummary("Reminder Set "+Long.valueOf(sharedPreferences.getString("reminder", "15"))+ " min before meeting");
-	
+		
+		ListPreference pref1 = (ListPreference)findPreference(DIGEST_LIST);
+		pref1.setSummary(pref1.getEntry());
+		
+		Preference logout = (Preference)findPreference(LOGOUT);
+		logout.setOnPreferenceClickListener(new Preference.OnPreferenceClickListener() {
+		                @Override
+		                public boolean onPreferenceClick(Preference arg0) { 
+		                	pr= getSharedPreferences(PREFS_NAME, Context.MODE_PRIVATE);
+		                	SharedPreferences.Editor editor = pr.edit();
+		                	editor.putString("logged", "not logged");
+							editor.commit();
+		                	Intent i = new Intent(Settings.this,Login.class);
+		            			startActivity(i); 
+		                    return true;
+		                }
+		            });
+		
+		Preference dropgroup = (Preference)findPreference(DROP);
+		dropgroup.setOnPreferenceClickListener(new Preference.OnPreferenceClickListener() {
+		                @Override
+		                public boolean onPreferenceClick(Preference arg0) { 
+		                    //code for what you want it to do   
+		                    return true;
+		                }
+		            });
+		Preference displaygroup = (Preference)findPreference(GROUPS);
+		displaygroup.setOnPreferenceClickListener(new Preference.OnPreferenceClickListener() {
+		                @Override
+		                public boolean onPreferenceClick(Preference arg0) { 
+		                	Intent i = new Intent(Settings.this,Get_Groups.class);
+	            			startActivity(i);   
+		                    return true;
+		                }
+		            });
+		Preference changecalendar = (Preference)findPreference(CHANGE);
+		changecalendar.setOnPreferenceClickListener(new Preference.OnPreferenceClickListener() {
+		                @Override
+		                public boolean onPreferenceClick(Preference arg0) { 
+		                	pr = getSharedPreferences(CALENDAR_CHOSEN, Context.MODE_PRIVATE);
+		                	SharedPreferences.Editor editor = pr.edit();
+		                	editor.putString("chosen", "not chosen");
+							editor.commit();
+		                	Intent i = new Intent(Settings.this,MyCalendars.class);
+		            		startActivity(i);    
+		                    return true;
+		                }
+		            });
 		
 	}
 	@Override
@@ -43,31 +104,36 @@ public class Settings extends PreferenceActivity implements OnSharedPreferenceCh
 	@Override
 	public void onSharedPreferenceChanged(SharedPreferences sharedPrefs, String key) {
 
-		
+		// Redundant with a switchpreference.
 		if (key.equals(ACTIVATE_SERVICE)) {
 			boolean active = sharedPrefs.getBoolean(ACTIVATE_SERVICE, true);
 			if (active) {
-				Toast.makeText(getBaseContext(), 
-		                "Service active",Toast.LENGTH_SHORT).show();
 				Reminder.start(getApplicationContext());
 			} else {
-				Toast.makeText(getBaseContext(), 
-		                "Service not active",Toast.LENGTH_SHORT).show();
 				Reminder.stop(getApplicationContext());
 			}
 		} 
+		
+		if (key.equals(DIGEST_LIST)) {
+			 ListPreference pref = (ListPreference)findPreference(key);
+			 pref.setSummary(pref.getEntry());
+			 
+			boolean active = sharedPrefs.getBoolean(DIGEST_CHECKBOX, true);
+			if (!active) {
+				Toast.makeText(getBaseContext(), 
+		                "Digest off",Toast.LENGTH_SHORT).show();
+			}
+		}
 		if (key.equals(REMINDER)) {
 			 Preference pref = findPreference(key);
 			 pref.setSummary("Reminder Set "+Long.valueOf(sharedPrefs.getString("reminder", "15"))+ " min before meeting");
 			boolean active = sharedPrefs.getBoolean(ACTIVATE_SERVICE, true);
 			if (active) {
-				Toast.makeText(getBaseContext(), 
-		                "Servic active",Toast.LENGTH_SHORT).show();
 				Reminder.restart(getApplicationContext());
 			}
 			else{
 				Toast.makeText(getBaseContext(), 
-		                "Service not active",Toast.LENGTH_SHORT).show();
+		                "Notification off",Toast.LENGTH_SHORT).show();
 			}
 		} 
 		
